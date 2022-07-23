010d7d061a302db1893bce0cf617a39e6d6bad10
JunaYa <ayac3j@gmail.com>
2022-07-23 18:16:33 +0800

    feat: create todo

--- src/pages/things/Index.vue
+++ src/pages/things/Index.vue
@@ -25,8 +25,33 @@ nav.init()
       </footer>
     </template>
     <template #main >
-      <div class="other">
-        content
+      <div class="other" p-12 style="background: rgb(247, 247, 249)">
+        <div bg-white p-6 rounded-sm>
+          <div frs>
+            <input self-start mt-1 type="checkbox" mr-2 class="default:ring-2 ..." />
+            <div flex-1 fcs>
+              <input type="text" placeholder="New To-Do" block outline-none class="default:ring-2 ..." />
+              <input type="text" placeholder="Notes" block outline-none mb-4 class="default:ring-2 ..." />
+              <div frb>
+                <button>tody</button>
+                <div flex flex-row items-center justify-end>
+                  <button class="icon-btn mx-2 !outline-none" >
+                    <div i="carbon-calendar" />
+                  </button>
+                  <button class="icon-btn mx-2 !outline-none" >
+                    <div i="carbon-tag" />
+                  </button>
+                  <button class="icon-btn mx-2 !outline-none" >
+                    <div i="carbon-list" />
+                  </button>
+                   <button class="icon-btn mx-2 !outline-none" >
+                    <div i="carbon-flag" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <footer frc>
         <button class="icon-btn" i="ph:plus-bold">+</button>
