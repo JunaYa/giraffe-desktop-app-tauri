0aeb4f4ecbca6ea65ce7da7155eadc33322eb329
YajunWang <wangyajun@hetao101.com>
2022-08-01 15:56:55 +0800

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -53,16 +53,25 @@ export default defineConfig({
     Legacy(),
   ],
 
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+
+  // Tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 8080,
-    host: '0.0.0.0',
-    proxy: {
-      '/api/': {
-        target: 'https://api.testing1.hetao101.com',
-        changeOrigin: true,
-        rewrite: p => p.replace(/^\/api-prod/, ''),
-      },
-    },
+    strictPort: true,
+  },
+
+  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+  // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+  // env variables
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri supports es2021
+    target: ['es2021', 'chrome100', 'safari13'],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 
   // https://github.com/vitest-dev/vitest
